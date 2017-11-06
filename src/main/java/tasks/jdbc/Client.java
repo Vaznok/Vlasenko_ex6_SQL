3b174b67116f8bc7baa88f3761189c9c7114c253
Vetall <vlasenko.vitaliy.l@gmail.com>
2017-11-06 20:41:05 +0200

    delet only students

--- src/main/java/tasks/jdbc/Client.java
+++ src/main/java/tasks/jdbc/Client.java
@@ -25,7 +25,7 @@ public class Client {
         }
         System.out.println();
         conn.commit();
-        stmt.execute("DELETE FROM GROUPS WHERE NAME='SR-01'");
+        stmt.execute("DELETE FROM STUDENTS WHERE GROUP_ID = 1");
 
         ResultSet rs2 = stmt.executeQuery("SELECT STUDENTS.FIRST_NAME, STUDENTS.LAST_NAME, GROUPS.NAME FROM STUDENTS " +
                 "INNER JOIN GROUPS ON STUDENTS.GROUP_ID=GROUPS.GROUP_ID");
